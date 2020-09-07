052f082fd13468a1b899048ac65dc76f412a6514
Raulks <rksole90@gmail.com>
2020-09-07 18:12:20 +0200

    clickout

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-
+//Abrir y cerrar divNavContainer
 abrirYcerrar = () => {
   let iconoMenu = document.querySelector(".hamburguer>img");
-  let navContainer = document.querySelector(".divNavContainer");
+  let navContainer = document.querySelector("#divNavContainer");
 
   if (iconoMenu.getAttribute("src") == "./img/icono-menu.png") {
     iconoMenu.setAttribute("src", "./img/close-menu.png");
-    navContainer.style.left = "0vw";
+    navContainer.style.left = "0";
   } else {
     iconoMenu.setAttribute("src", "./img/icono-menu.png");
-    navContainer.style.left = "-58vw";
+    navContainer.style.left = "-59vw";
   }
 };
-
+//Desplegar secciones divNavContainer
 openDropDown = (position) => {
   let arrayOfUls = [...document.querySelectorAll(".dropDownLi>ul")];
 
@@ -21,5 +21,18 @@ openDropDown = (position) => {
     arrayOfUls[position].style.display = "block";
   } else {
     arrayOfUls[position].style.display = "none";
+  }
+};
+
+//Cerrar al clickar fuera del divNavContainer
+window.onclick = (event) => {
+  let iconoMenu = document.querySelector(".hamburguer>img");
+  let navContainer = document.querySelector("nav > div").contains(event.target);
+
+  if (!navContainer) {
+    if (document.querySelector("#divNavContainer")) {
+      document.querySelector("#divNavContainer").style.left = "-59vw";
+      iconoMenu.setAttribute("src", "./img/icono-menu.png");
+    }
   }
 };
